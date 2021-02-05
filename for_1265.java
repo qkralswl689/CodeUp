97b2aa7b42030834e8d986244c3847439e9dd1fb
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-05 23:20:06 +0900

    02.05

---
+++ for_1265.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class for_1265 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i=a; i<=a; i++) {
+			for(int j= 1; j<=9; j++) {
+				System.out.printf("%d*%d=%d%n",i,j,i*j);
+			}
+			
+		}
+
+	}
+
+}
