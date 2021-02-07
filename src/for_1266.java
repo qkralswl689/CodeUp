69db470ec4446b918c6caf2bff16ccee04268651
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-07 23:14:12 +0900

    02.07

--- src/for_1266.java
+++ src/for_1266.java
@@ -3,15 +3,17 @@ public class for_1266 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-//		int a = sc.nextInt();
-		int n = sc.nextInt();
-		int sum = 0;
-		for(int i=0;i<n;i++) {
-			sum += sc.nextInt();
+		Scanner sc = new Scanner(System.in); // 입력 클래스인 Scanner 객체 생성
+		int n = sc.nextInt(); // 입력된 값 n에 저장
+		int sum = 0; // 합계값 저장할 변수 선언
+		
+		for(int i=1; i<=n; i++) { // 1부터 입력받은 n까지 1씩 증가하면서 반복
 			
-		}
-		System.out.println(sum);
-	}
+			sum += sc.nextInt(); // i의 값 많큼 숫자를 입력받아 sum에  입력받은 숫자의 합계 저장
+		
+		}System.out.println(sum); // 입력받은 숫자의 합계 출력
+		
 
+	}
 }
+	
---
+++ src/for_1268.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class for_1268 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // 입력 클래스 Scanner 객체 생성
+		int n = sc.nextInt(); // 값 입력받아 변수 n에 저장
+		int sum = 0; // n의 값만큼 입력된 값 sum변수에 저장
+		int count = 0; // 조건에 맞는 값 count변수에 저장
+		for(int i=1;i<=n; i++) { // 입력받은 n의 값만큼 정수를 입력받아 sum변수에 저장
+			sum = sc.nextInt();
+			if(sum%2==0) { // sum에 저장된 값 중에 짝수가 있다면
+				count++; // count변수에  증감식으로  저장
+				
+			}
+		}System.out.println(count); // count변수의 값 출력
+
+	}
+
+}
