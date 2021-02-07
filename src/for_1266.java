5610406852a0249ef3075e92d5f8a2fdcf1fb961
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-07 18:35:43 +0900

    move

---
+++ src/for_1266.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class for_1266 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i=0;i<n;i++) {
+			sum += sc.nextInt();
+			
+		}
+		System.out.println(sum);
+	}
+
+}
