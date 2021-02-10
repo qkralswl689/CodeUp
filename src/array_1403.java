c66b7534c7dcad1ee8f2194f8301fcad9f96bf2f
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-10 23:13:40 +0900

    02.10

---
+++ src/array_1403.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class array_1403 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+			
+		
+	}
+
+}
