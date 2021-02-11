790297fb8e6abc21db555919807a7dc0574b7418
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-11 20:48:55 +0900

    Update array_1405_answer.java

--- src/array_1405_answer.java
+++ src/array_1405_answer.java
@@ -13,13 +13,14 @@ public class array_1405_answer {
 		int tmp=0;
 		for(int i=0;i<arr.length; i++) {
 			tmp = i;
-			
+			//System.out.print(i);
 			for(int j=0; j<arr.length;j++) {
-				if(tmp != arr.length-1) {
+				if(tmp != arr.length-1) { // i 가 배열의 끝자리와 다르다면
+					//System.out.print(arr[tmp++]);
 					System.out.print(arr[tmp++]);
 				}else if(tmp == arr.length-1) {
 					System.out.print(arr[tmp]);
-					tmp =0;
+					tmp =0; // 초기화
 				}
 			}System.out.println();
 		}
