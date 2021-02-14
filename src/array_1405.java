842c2957bf871f0ed16b4db80c54f017e838c653
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-14 22:10:04 +0900

    02.14

--- src/array_1405.java
+++ src/array_1405.java
@@ -10,21 +10,7 @@ public class array_1405 {
 		for(int i=0;i<n;i++) {
 			arr[i] = sc.nextInt();
 		}
-		int tmp=0;
-		for(int i=0; i<arr.length;i++) {
-			tmp =i;
-			for(int j =0; j<arr.length; j++) {
-				if(tmp != arr.length-1) {
-					System.out.print(arr[tmp++]);
-				}else if(tmp == arr.length-1) {
-					System.out.print(arr[tmp]);
-					tmp=0;
-			}
-			
-			}System.out.println();
-		}
 		
-			
 	}
 		
 }
