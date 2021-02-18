c8ce06348fdaa7aa96277823099ae30b9271a22d
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-18 22:28:05 +0900

    02.18

---
+++ src/String_1407.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class String_1407 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // 입력받을 Scanner 객체 생성
+
+		String str = sc.nextLine(); // 입력받은 문자열을 str변수에 저장한다
+		
+		String[] arr = str.split(" "); // 문자열 배열 arr에 str에 저장된 문자열을  " "을 기준으로 각각 분리하여 배열에 할당 
+		
+		for(int i =0; i<arr.length; i++) { // i는 0부터 배열의 길이만큼 1씩증가하면서 반복한다
+			System.out.print(arr[i]); // 뱅여 출력
+		}
+ 	}
+
+}
