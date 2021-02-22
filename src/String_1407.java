a00ccaf80b49796de5a25eef06bb5d6176cebefc
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-02-22 17:38:35 +0900

    02.22

--- src/String_1407.java
+++ src/String_1407.java
@@ -10,7 +10,7 @@ public class String_1407 {
 		String[] arr = str.split(" "); // 문자열 배열 arr에 str에 저장된 문자열을  " "을 기준으로 각각 분리하여 배열에 할당 
 		
 		for(int i =0; i<arr.length; i++) { // i는 0부터 배열의 길이만큼 1씩증가하면서 반복한다
-			System.out.print(arr[i]); // 뱅여 출력
+			System.out.print(arr[i]); // 배열 출력
 		}
  	}
 
